582d7f27ef7da4c4d4767bf768189367cbb8d865
li220fish <li220fish@gmail.com>
2024-11-13 21:41:57 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 13 15:59:43 2024
+
+@author: LiFish
+"""
+
+from flask import Flask
+
+app = Flask(__name__,static_folder='frontend/build',static_url_path='/')
+
+@app.route('/')
+def index():
+    return app.send_static_file("index.html")
+
+if __name__ == "__main__":
+    app.run("0.0.0.0",port=5000)
---
+++ frontend/src/components/CityData.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import City from "./City";
+import "./CityStyles.css";
+
+function CityData(props) {
+  return (
+    <div className="c-card">
+      <div className="c-image">
+        <img src={props.image} alt="image" />
+      </div>
+      <h4>{props.heading}</h4>
+      <p>{props.text}</p>
+    </div>
+  );
+}
+
+export default CityData;
