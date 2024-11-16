8de7ea876e23d912ed9fe36d2db494515f6603db
Li220Fish <119789640+Li220Fish@users.noreply.github.com>
2024-11-17 01:35:40 +0800

    Update CityData.js

--- frontend/src/components/CityData.js
+++ frontend/src/components/CityData.js
@@ -1,15 +1,25 @@
+// frontend/src/CityData.js
 import React from 'react';
-import City from "./City";
+import { useNavigate } from 'react-router-dom';
 import "./CityStyles.css";
 
 function CityData(props) {
+  const navigate = useNavigate(); // 使用 useNavigate 來處理跳轉
+
+  // 點擊事件處理
+  const handleClick = () => {
+    console.log("handleClick Ture")
+    navigate('/restaurant'); // 跳轉至 /service 頁面
+
+  };
+
   return (
-    <div className="c-card">
+    <div className="c-card" onClick={handleClick}>
       <div className="c-image">
         <img src={props.image} alt="image" />
+        <div className="c-caption">{props.heading}</div>
       </div>
-      <h4>{props.heading}</h4>
-      <p>{props.text}</p>
+      {props.text && <p className="c-text">{props.text}</p>}
     </div>
   );
 }
