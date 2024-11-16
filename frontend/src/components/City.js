30c7cc73905548b87b634c9a542c0958e773c118
Li220Fish <119789640+Li220Fish@users.noreply.github.com>
2024-11-17 01:35:22 +0800

    Update City.js

--- frontend/src/components/City.js
+++ frontend/src/components/City.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import "./CityStyles.css";
 import CityData from "./CityData";
-import City1 from "../assets/manila1.jpg";
-import City2 from "../assets/visayas.jpg";
-import City3 from "../assets/mindanao.jpg";
+import City1 from "../assets/tapei.webp";
+import City2 from "../assets/new_taipei.webp";
+import City3 from "../assets/taoyuan.webp";
+import City4 from "../assets/hsinchu.webp";
+import City5 from "../assets/taichung.webp";
+import City6 from "../assets/changhua.webp";
+import City7 from "../assets/tainan.webp";
+import City8 from "../assets/kaohsiung.webp";
+import City9 from "../assets/pingtung.webp";
 
 function City() {
   return (
     <div className="city">
-      <h1>Reaching More Cities</h1>
-      <p>Find us in the cities and many more!</p>
+      <h2>我們有在您所在的城市提供送餐服務</h2>
       <div className="citycard">
-        <CityData image={City1} heading="Manila" />
-        <CityData image={City2} heading="Visayas" />
-        <CityData image={City3} heading="Mindanao" />
+        <CityData image={City1} heading="台北市" />
+        <CityData image={City2} heading="新北市" />
+        <CityData image={City3} heading="桃園市" />
+        <CityData image={City4} heading="新竹市" />
+        <CityData image={City5} heading="台中市" />
+        <CityData image={City6} heading="彰化市" />
+        <CityData image={City7} heading="台南市" />
+        <CityData image={City8} heading="高雄市" />
+        <CityData image={City9} heading="屏東縣" />
       </div>
     </div>
   );
