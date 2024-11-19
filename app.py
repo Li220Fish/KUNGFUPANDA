530633c9b1874e651424c0625857eb09437146b6
Li220Fish <119789640+Li220Fish@users.noreply.github.com>
2024-11-20 01:36:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def index():
     return app.send_static_file("index.html")
 @app.route('/restaurant')
 def index():
-    return "helloaaa"
+    return "hello"
 
 if __name__ == "__main__":
     app.run("0.0.0.0",port=5000)
