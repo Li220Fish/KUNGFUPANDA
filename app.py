281204d246372a4738560e17cec77dd6c917f115
li220fish <li220fish@gmail.com>
2024-11-20 01:31:23 +0800

    43e34355

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ app = Flask(__name__,static_folder='frontend/build',static_url_path='/')
 @app.route('/')
 def index():
     return app.send_static_file("index.html")
+@app.route('/restaurant')
+def index():
+    return "hello"
 
 if __name__ == "__main__":
     app.run("0.0.0.0",port=5000)
