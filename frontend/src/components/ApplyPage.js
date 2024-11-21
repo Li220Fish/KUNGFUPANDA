012bb604b4a0d33212c0c18f2b333c6e778bfe3d
鑫 <bensonb921122@gmail.com>
2024-11-21 22:26:41 +0800

    Update ApplyPage.js

--- frontend/src/components/ApplyPage.js
+++ frontend/src/components/ApplyPage.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import './ApplyPageStyles.css';
+import foodpandaLogo from '../assets/foodpanda_logo.jfif';
 
-function ApplyPage() {
+const ApplyPage = () => {
+  const handleButtonClick = () => {
+    // 在這裡添加按鈕點擊的功能
+    console.log('Button clicked!');
+  };
+
   return (
-    <div>
-      <h1>應徵頁面</h1>
-      {/* 其他內容 */}
+    <div className="apply-page">
+      <img src={foodpandaLogo} alt="Food Panda Logo" className="logo" />
+      <button className="apply-button" onClick={handleButtonClick}>
+        ZH
+      </button>
     </div>
   );
-}
+};
 
 export default ApplyPage;
