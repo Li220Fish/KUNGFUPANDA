97aed2923ee5ecc94be650c28fd33fa3b2b93645
鑫 <bensonb921122@gmail.com>
2024-11-26 18:39:30 +0800

    update applypage

--- frontend/src/components/ApplyPage.js
+++ frontend/src/components/ApplyPage.js
@@ -7,6 +7,10 @@ import foodpandaFeature2 from '../assets/motowomen.jpg_27-11-2023-08_03_23';
 import cash from '../assets/cash.svg_21-06-2021-09_52_43';
 import firstaidkit from '../assets/firstaidkit.svg_21-06-2021-09_52_43';
 import balloon from '../assets/balloon.svg_21-06-2021-09_52_43';
+import mrslin from '../assets/mrslin.jpg_01-07-2021-09_05_19';
+import mrlin from '../assets/mrlin.jpg_01-07-2021-09_05_32';
+import footpic from '../assets/footpic.jpg_27-11-2023-08_03_22';
+import footerpic from '../assets/footerpic.png';
 
 
 const ApplyPage = () => {
@@ -117,10 +121,65 @@ const ApplyPage = () => {
           </div>
         </div>
       </div>
+      <div className='applyfoot'>
+        <h2>申請條件簡單快速，foodpanda 成為你的新支柱！</h2>
+        <ul>
+        　<li>年滿18歲，具備台灣身分證</li>
+        　<li>本人之「台幣」帳戶（可擇一使用中國信託銀行、國泰世華銀行、玉山商業銀行、台新國際商業銀行、中華郵政、台灣銀行）</li>
+        　<li>機車、機車駕照、機車強制險卡</li>
+        　<li>含網路的智慧型手機</li>
+        　<li>警察刑事紀錄證明（良民證）</li>
+        </ul>
+      </div>
 
 
 
+      <div className="deliveryparners">
+          <h2>外送夥伴故事</h2>
+      
 
+        <div className="parners">
+          <div className="mrslin">
+            <div className="mrslin-text">
+              <img src={mrslin} alt="mrslin_img" className="mrslinpic" />
+              <h2>外送夥伴林小姐</h2>
+              <p>支持她擁抱斜槓人生的，正是foodpanda的外送工作! 除了外送員的身分，我同時也是一名藝術工作者，平時會接一些視覺設計、影像攝影的案子。自由彈性的工作特性，讓我能夠做自己喜歡的事。 剛開始幾乎天天上線，一天大概跑8小時。雖然有點辛苦但遇過很多消費者會送水或飲料。這些鼓勵都讓我更有動力堅持下去。外送的報酬讓我不必再擔心生活，更可以專注地投入創作。</p>
+
+            </div>
+            
+          </div>
+
+          <div className="mrlin">
+            <div className="mrlin-text">
+              <img src={mrlin} alt="mrlin_img" className="mrlinpic" />
+              <h2>外送夥伴林先生</h2>
+              <p>可以自由地騎車又可以存錢，沒有比這更開心的事了! 每天開工前，我會做好萬全的準備，像是把油加滿、零錢備妥。 跑過其他外送平台，最後選擇留在foodpanda服務，主因是合作的餐廳較多、不需跨區外送，尤其是「選擇時段上線」的制度，讓外送員可以維持一定的單量，不會互相搶單，單量與報酬都更穩定、更有保障。</p>
+            </div>
+          </div>
+
+          
+        </div>
+      </div>
+
+
+      <div className='footpng'>
+        <img src={footpic} alt="footpic_img" className="footpic" />
+        <h2>實踐自由的人生，就選 foodpanda 外送工作！</h2>
+      </div>
+
+
+      <div className='footerpng'>
+        <div className="footerleft">
+          <img src={footerpic} alt="footpic_img" className="footerpic" />
+        </div>
+        <div className="footerright">
+          <h2>了解更多</h2>
+          <p>開通懶人包</p>
+          <p1>申請開通助手 官方</p1>
+          <p1>LINE</p1>
+        </div>
+
+      </div>
 
 
     </div>
